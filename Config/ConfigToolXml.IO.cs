79189f3f3ad317401c161f178cef5045ea752cae
River Mochi <97197236+River-Mochi@users.noreply.github.com>
2026-06-22 16:07:37 -0700

    add copywrite block to files

--- Config/ConfigToolXml.IO.cs
+++ Config/ConfigToolXml.IO.cs
@@ -1,3 +1,11 @@
+// <copyright file="ConfigToolXml.IO.cs" company="River-Mochi">
+// Copyright (c) 2026 River-Mochi. All rights reserved.
+// Licensed under the MIT License. You may not use this file except in compliance with this License.
+// See LICENSE file in the project root for full license information.
+// This notice and the MIT License notice must be kept with
+// all copies or substantial portions of this code.
+// ================= </copyright> ======================
+
 // File: Config/ConfigToolXml.IO.cs
 // Purpose: ConfigToolXml IO + migration + load/save helpers for Config-XML.
 // Notes:
